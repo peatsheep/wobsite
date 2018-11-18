b5671f9f926e05f8c588f40214261a0cbed1e479
Will Neville <william@neville.com>
2018-11-17 21:57:14 -0500

    Updates

---
+++ precache-manifest.d5d73128263ac3fafae57e7a85ae1ee9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c5a7c8fa828f571b43f",
+    "url": "/wobsite/static/css/main.bf750f05.chunk.css"
+  },
+  {
+    "revision": "3c5a7c8fa828f571b43f",
+    "url": "/wobsite/static/js/main.3c5a7c8f.chunk.js"
+  },
+  {
+    "revision": "13a49096ab8052b2fe7c",
+    "url": "/wobsite/static/js/1.13a49096.chunk.js"
+  },
+  {
+    "revision": "76ab27e04a4ed13c6a4c",
+    "url": "/wobsite/static/js/runtime~main.76ab27e0.js"
+  },
+  {
+    "revision": "38f27e23860e6e984b2817ae104b353a",
+    "url": "/wobsite/index.html"
+  }
+];
